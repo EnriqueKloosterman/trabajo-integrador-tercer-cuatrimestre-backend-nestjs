f798bd02edd55f1cc802ff9dd22864cd96fbc768
Malvina14 <malvinapacheco61rgmail.com>
2023-08-24 17:23:25 -0300

    Controlador de article

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
 
-@Controller('article')
-export class ArticleController {}
+import { Controller, Get, Param, Post, Body } from '@nestjs/common';
+import { Article } from './article.interface';
+import { ArticleService } from './article.service';
+
+@Controller('articles')
+export class articleController {
+  constructor(private readonly articleService: ArticleService) {}
+  
+  @Get()
+  getArticles(): Promise<Article[]> {
+    return this.articleService.getArticles();
+  }
+  @Get('/:id')
+  getArticleById(@Param('id') id: number): any {
+    return this.articleService.getArticleById(id);
+  }
+  @Post()
+  createArticle(@Body() article: Article): any {
+    return this.articleService.createArticle(article);
+  }
+}
+ 
+
+  
+
+
+
+
--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
 
-@Injectable()
-export class ArticleService {}
+
+export class ArticleService {
+  async getArticles(): Promise<Article[]> {
+    const res = await fetch(URL);
+    const parsed = await res.json();
+    return parsed;
+  }
+  async getArticleById(id: number): Promise<Article> {
+    const res = await fetch(`${URL}${id}`);
+    const parsed = await res.json();
+    return parsed;
+  }
+  async createArticle(article: Article): Promise<Article> {
+    const id = await this.createId();
+    const articleId = { id, ...Article };
+    const res = await fetch(URL, {
+      method: 'POST',
+      body: JSON.stringify(articleId),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const parsed = await res.json();
+    return parsed;
+  }
+  async createId(): Promise<number> {
+    const res = await this.getArticles();
+    const lastArticle = res[res.length - 1];
+    const id = lastArticle.id + 1;
+    return id;
+  }
+}
