5f0dde403bf2e4138c872b5836c730425d9f9449
EnriqueKloosterman <kloostermanen@gmail.com>
2023-08-25 11:52:31 -0300

    Agrergados getAllRecipes, getRecipesById y createRecipes al recipeService y controller

--- src/recipe/recipe.controller.ts
+++ src/recipe/recipe.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body } from '@nestjs/common';
+import { RecipeService } from './recipe.service';
+import { Recipe } from './recipe.interface';
 
 @Controller('recipe')
-export class RecipeController {}
+export class RecipeController {
+  constructor(private readonly recipeService: RecipeService) {}
+  @Get()
+  getAllRecipes(): Promise<Recipe[]> {
+    return this.recipeService.getAllRecipes();
+  }
+  @Get(':id')
+  getRecipeById(@Param('id') id: number): Promise<Recipe> {
+    return this.recipeService.getRecipeById(id);
+  }
+  @Post()
+  createRecipe(@Body() recipe: Recipe): Promise<Recipe> {
+    return this.recipeService.createRecipe(recipe);
+  }
+}
+
--- src/recipe/recipe.service.ts
+++ src/recipe/recipe.service.ts
@@ -1,4 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { Recipe } from './recipe.interface';
+const recipeURL = 'http://localhost:3030/recipe/';
 
 @Injectable()
-export class RecipeService {}
+export class RecipeService {
+  async getAllRecipes(): Promise<Recipe[]> {
+    const res = await fetch(recipeURL);
+    const parsed = await res.json();
+    return parsed;
+  }
+  async getRecipeById(id: number): Promise<Recipe> {
+    const res = await fetch(`${recipeURL}/${id}`);
+    const parsed = await res.json();
+    return parsed;
+  }
+  async createRecipe(recipe: Recipe): Promise<Recipe> {
+    const id = await this.createId();
+    const recipeWithId = { id, ...recipe };
+    const res = await fetch(recipeURL, {
+      method: 'POST',
+      body: JSON.stringify(recipeWithId),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const parsed = await res.json();
+    return parsed;
+  }
+  async createId(): Promise<number> {
+    const res = await this.getAllRecipes();
+    const lastRecipe = res[res.length - 1];
+    const id = lastRecipe.id + 1;
+    return id;
+  }
+}
