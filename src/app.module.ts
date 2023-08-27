be5b8bbf36dfff250c673b983ca43eaab39ee3fe
EnriqueKloosterman <kloostermanen@gmail.com>
2023-08-27 11:07:23 -0300

    Modulos users, recipe, coments y article agregados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,10 @@
 import { Module } from '@nestjs/common';
-import { UserController } from './user/user.controller';
-import { UserService } from './user/user.service';
-import { RecipeController } from './recipe/recipe.controller';
-import { RecipeService } from './recipe/recipe.service';
-import { ComentsController } from './coments/coments.controller';
-import { ComentsService } from './coments/coments.service';
-import { ArticleController } from './article/article.controller';
-import { ArticleService } from './article/article.service';
+import { UserModule } from './user/user.module';
+import { RecipeModule } from './recipe/recipe.module';
+import { ComentsModule } from './coments/coments.module';
+import { ArticleModule } from './article/article.module';
 
 @Module({
-  imports: [],
-  controllers: [
-    UserController,
-    RecipeController,
-    ComentsController,
-    ArticleController,
-  ],
-  providers: [UserService, RecipeService, ComentsService, ArticleService],
+  imports: [UserModule, RecipeModule, ComentsModule, ArticleModule],
 })
 export class AppModule {}
