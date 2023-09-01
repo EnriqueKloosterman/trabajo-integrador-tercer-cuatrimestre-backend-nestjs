1b99a2716171397d9b4eed1265e467b5e9e7da82
Florencia Da Rosa <floordarosa2002gmail.com>
2023-09-01 16:35:01 -0300

    se agrega decorador res a article y coments

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,24 +1,44 @@
-import { Controller, Get, Param, Post, Body, Delete, Put, HttpCode } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Delete, Put, HttpCode, Res, HttpStatus, BadRequestException } from '@nestjs/common';
 import { Article } from './article.interface';
 import { ArticleService } from './article.service';
+import { Response } from 'express';
 
 @Controller('articles')
 export class ArticleController {
   constructor(private readonly articleService: ArticleService) {}
 
   @Get()
-  getArticles(): Promise<Article[]> {
-    return this.articleService.getArticles();
+  async getArticles(@Res() res: Response): Promise<any> {
+    try {
+      const articles = await this.articleService.getArticles();
+      return res.status(HttpStatus.OK).json(articles);
+    } catch (error) {
+      return res.status(HttpStatus.BAD_REQUEST).json(error);
+    }
   }
 
   @Get('/:id')
-  getArticleById(@Param('id') id: number): Promise<Article> {
-    return this.articleService.getArticleById(id);
+  async getArticleById(@Param('id') id: number, @Res() res: Response): Promise<any> {
+    try {
+      const article = await this.articleService.getArticleById(id);
+      if (article) {
+        return res.status(HttpStatus.OK).json(article);
+      } else {
+        return res.status(HttpStatus.NOT_FOUND).json({ message: 'Article not found' });
+      }
+    } catch (error) {
+      throw new BadRequestException(`Article with id ${id} not found.`);
+    }
   }
 
   @Post()
-  createArticle(@Body() article: Article): Promise<Article> {
-    return this.articleService.createArticle(article);
+  async createArticle(@Body() article: Article, @Res() res: Response): Promise<any> {
+    try {
+      const createdArticle = await this.articleService.createArticle(article);
+      return res.status(HttpStatus.CREATED).json(createdArticle);
+    } catch (error) {
+      throw new BadRequestException('Article creation failed');
+    }
   }
 
   @Delete('/:id')
--- src/coments/coments.controller.ts
+++ src/coments/coments.controller.ts
@@ -1,34 +1,65 @@
-import { Controller, Get, Param, Post, Body, Delete, Put, HttpCode } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Delete, Put, HttpCode, Res, HttpStatus, BadRequestException } from '@nestjs/common';
 import { Coments } from './coments.interface';
 import { ComentsService } from './coments.service';
+import { Response } from 'express';
 
 @Controller('coments')
 export class ComentsController {
   constructor(private readonly comentsService: ComentsService) {}
 
   @Get()
-  getComents(): Promise<Coments[]> {
-    return this.comentsService.getComents();
+  async getComents(@Res() res: Response): Promise<any> {
+    try {
+      const coments = await this.comentsService.getComents();
+      return res.status(HttpStatus.OK).json(coments);
+    } catch (error) {
+      return res.status(HttpStatus.BAD_REQUEST).json(error);
+    }
   }
 
-  @Get('/:id')
-  getComentsById(@Param('id') id: number): any {
-    return this.comentsService.getComentsById(id);
+  @Get(':id')
+  async getComentsById(@Param('id') id: number, @Res() res: Response): Promise<any> {
+    try {
+      const coments = await this.comentsService.getComentsById(id);
+      if (coments) {
+        return res.status(HttpStatus.OK).json(coments);
+      } else {
+        return res.status(HttpStatus.NOT_FOUND).json({ message: 'Coments not found' });
+      }
+    } catch (error) {
+      throw new BadRequestException(`Coments with id ${id} not found.`);
+    }
   }
 
   @Post()
-  createComents(@Body() coments: Coments): any {
-    return this.comentsService.createComents(coments);
+  async createComents(@Body() coments: Coments, @Res() res: Response): Promise<any> {
+    try {
+      const createdComents = await this.comentsService.createComents(coments);
+      return res.status(HttpStatus.CREATED).json(createdComents);
+    } catch (error) {
+      throw new BadRequestException('Coments creation failed');
+    }
   }
 
   @Delete('/:id')
-  deleteComents(@Param('id') id: number): any {
-    return this.comentsService.deleteComents(id);
+  @HttpCode(204)
+  async deleteComents(@Param('id') id: number, @Res() res: Response): Promise<any> {
+    try {
+      await this.comentsService.deleteComents(id);
+      return res.sendStatus(HttpStatus.NO_CONTENT);
+    } catch (error) {
+      throw new BadRequestException(`Coments with id ${id} not found.`);
+    }
   }
 
   @Put('/:id')
   @HttpCode(204)
-  updateComents(@Param('id') id: number, @Body() body: Coments): Promise<Coments> {
-    return this.comentsService.updateComents(id, body);
+  async updateComents(@Param('id') id: number, @Body() body: Coments, @Res() res: Response): Promise<any> {
+    try {
+      await this.comentsService.updateComents(id, body);
+      return res.sendStatus(HttpStatus.NO_CONTENT);
+    } catch (error) {
+      throw new BadRequestException(`Coments with id ${id} not found.`);
+    }
   }
 }
