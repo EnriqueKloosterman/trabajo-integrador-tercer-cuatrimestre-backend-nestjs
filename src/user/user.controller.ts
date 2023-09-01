30fee101daba1efa11a53330fe062278147dd168
EnriqueKloosterman <kloostermanen@gmail.com>
2023-09-01 10:15:21 -0300

    agregados codigos de response a users

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -7,26 +7,53 @@ import {
   Delete,
   Put,
   HttpCode,
+  Res,
+  HttpStatus,
+  BadRequestException,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './user.interface';
+import { Response } from 'express';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
   @Get()
-  getUser(): Promise<User[]> {
-    return this.userService.getUsers();
+  async getUser(@Res() res: Response): Promise<any> {
+    try {
+      const serviceResponse = await this.userService.getUsers();
+      return res.status(HttpStatus.OK).json(serviceResponse);
+    } catch (error) {
+      return res.status(HttpStatus.BAD_REQUEST).json(error);
+    }
   }
   @Get('/:id')
-  getUserById(@Param('id') id: number): any {
-    return this.userService.getUserById(id);
+  async getUserById(
+    @Param('id') id: number,
+    @Res() res: Response,
+  ): Promise<any> {
+    try {
+      const serviceResponse = await this.userService.getUserById(id);
+      if (Object.keys(serviceResponse).length) {
+        return res.status(HttpStatus.OK).send(serviceResponse);
+      } else {
+        return res.status(HttpStatus.NOT_FOUND).send(serviceResponse);
+      }
+    } catch (error) {
+      throw new BadRequestException(`User with id ${id} not found`);
+    }
   }
   @Post()
-  createUser(@Body() user: User): any {
-    return this.userService.createUser(user);
+  async createUser(@Body() user: User, @Res() res: Response): Promise<any> {
+    try {
+      const serviceResponse = await this.userService.createUser(user);
+      await res.status(HttpStatus.CREATED).send(serviceResponse);
+    } catch (error) {
+      throw new BadRequestException(`User creation has failed`);
+    }
   }
   @Delete('/:id')
+  @HttpCode(204)
   deleteUser(@Param('id') id: number): any {
     return this.userService.deleteUser(id);
   }
