f9a9e3e605a27c8e318d0de6eceb987e1db98908
Florencia Da Rosa <floordarosa2002gmail.com>
2023-09-07 16:41:50 -0300

    se agrega dto de article

--- backend/src/article/article.controller.ts
+++ backend/src/article/article.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Post, Body, Delete, Put, HttpCode, Res, HttpStatus, BadRequestException } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Delete, Put, HttpCode, Res, HttpStatus, HttpException } from '@nestjs/common';
 import { Article } from './article.interface';
 import { ArticleService } from './article.service';
 import { Response } from 'express';
@@ -10,36 +10,23 @@ export class ArticleController {
 
   @Get()
   async getArticles(@Res() res: Response): Promise<any> {
-    try {
-      const articles = await this.articleService.getArticles();
-      return res.status(HttpStatus.OK).json(articles);
-    } catch (error) {
-      return res.status(HttpStatus.BAD_REQUEST).json(error);
-    }
+    const articles = await this.articleService.getArticles();
+    return res.status(HttpStatus.OK).json(articles);
   }
 
   @Get('/:id')
   async getArticleById(@Param('id') id: number, @Res() res: Response): Promise<any> {
-    try {
-      const article = await this.articleService.getArticleById(id);
-      if (article) {
-        return res.status(HttpStatus.OK).json(article);
-      } else {
-        return res.status(HttpStatus.NOT_FOUND).json({ message: 'Article not found' });
-      }
-    } catch (error) {
-      throw new BadRequestException(`Article with id ${id} not found.`);
+    const article = await this.articleService.getArticleById(id);
+    if (!article) {
+      throw new HttpException(`Article with id ${id} not found.`, HttpStatus.NOT_FOUND);
     }
+    return res.status(HttpStatus.OK).json(article);
   }
 
   @Post()
-  async createArticle(@Body() article: Article, @Res() res: Response): Promise<any> {
-    try {
-      const createdArticle = await this.articleService.createArticle(article);
-      return res.status(HttpStatus.CREATED).json(createdArticle);
-    } catch (error) {
-      throw new BadRequestException('Article creation failed');
-    }
+  async createArticle(@Body() createArticleDto: CreateArticleDto, @Res() res: Response): Promise<any> {
+    const createdArticle = await this.articleService.createArticle(createArticleDto);
+    return res.status(HttpStatus.CREATED).json(createdArticle);
   }
 
   @Delete('/:id')
--- backend/src/article/create-article.dto.ts
+++ backend/src/article/create-article.dto.ts
@@ -1,5 +1,5 @@
 import { Expose } from 'class-transformer';
-import { IsString, IsNotEmpty, IsArray } from 'class-validator';
+import { IsString, IsNotEmpty, IsArray, IsNumber, IsDate } from 'class-validator';
 
 export class CreateArticleDto {
   @Expose()
@@ -11,8 +11,24 @@ export class CreateArticleDto {
   @IsString()
   @IsNotEmpty()
   article: string;
-  
+
   @Expose()
   @IsArray()
   ingredients: string[];
+
+  // Añadimos los decoradores IsNumber y IsDate para las nuevas propiedades
+  @Expose()
+  @IsNumber()
+  @IsNotEmpty()
+  user_id: number;
+
+  @Expose()
+  @IsDate()
+  @IsNotEmpty()
+  createdAt: Date;
+
+  @Expose()
+  @IsDate()
+  @IsNotEmpty()
+  updatedAt: Date;
 }
