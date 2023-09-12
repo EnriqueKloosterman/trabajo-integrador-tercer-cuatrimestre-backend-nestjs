c3ada1ca1fd06ca96add00cdbf9812e81bb75a9c
Florencia Da Rosa <floordarosa2002gmail.com>
2023-09-12 19:51:19 -0300

    se agrega color violeta a contacto

--- frontend/src/componentes/Contact.jsx
+++ frontend/src/componentes/Contact.jsx
@@ -22,15 +22,14 @@ function Contact() {
 
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-4xl font-bold text-lime-700 mb-8">Contáctanos</h1>
+      <h1 className="text-4xl font-bold text-violet-700 mb-8">Contáctanos</h1>
       <p className="text-lg text-gray-700">
         Si tienes preguntas, comentarios o sugerencias, no dudes en ponerte en contacto con nosotros. Estamos aquí para ayudarte en lo que necesites.
       </p>
       <p className="text-lg text-gray-700 mt-4">
-        Puedes enviarnos un correo electrónico a <a href="mailto:info@miblogdecocina.com" className="text-lime-500 hover:underline">info@miblogdecocina.com</a> o llenar el formulario de contacto a continuación.
+        Puedes enviarnos un correo electrónico a <a href="mailto:info@miblogdecocina.com" className="text-violet-500 hover:underline">info@miblogdecocina.com</a> o llenar el formulario de contacto a continuación.
       </p>
-      
-     
+
       <form onSubmit={handleSubmit} className="mt-6">
         <div className="mb-4">
           <label htmlFor="name" className="block text-gray-700 text-lg font-semibold">Nombre</label>
@@ -40,7 +39,7 @@ function Contact() {
             name="name"
             value={formData.name}
             onChange={handleChange}
-            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-lime-500"
+            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-violet-500"
             required
           />
         </div>
@@ -52,7 +51,7 @@ function Contact() {
             name="email"
             value={formData.email}
             onChange={handleChange}
-            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-lime-500"
+            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-violet-500"
             required
           />
         </div>
@@ -64,14 +63,14 @@ function Contact() {
             value={formData.message}
             onChange={handleChange}
             rows="4"
-            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-lime-500"
+            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-violet-500"
             required
           ></textarea>
         </div>
         <div className="text-center">
           <button
             type="submit"
-            className="bg-lime-500 hover:bg-lime-600 text-white font-semibold px-6 py-3 rounded-lg transition-colors duration-300"
+            className="bg-violet-500 hover:bg-violet-600 text-white font-semibold px-6 py-3 rounded-lg transition-colors duration-300"
           >
             Enviar Mensaje
           </button>
@@ -82,4 +81,5 @@ function Contact() {
 }
 
 export default Contact;
+
 
