b22c4fe4bd66cbe22c5a3a4b6dafbf41e19ab22a
Florencia Da Rosa <floordarosa2002gmail.com>
2023-09-14 13:58:41 -0300

    se agregan estilos a article Detail

--- frontend/src/componentes/ArticleDetail.jsx
+++ frontend/src/componentes/ArticleDetail.jsx
@@ -2,40 +2,47 @@ import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 
 function ArticleDetail() {
-    const { id } = useParams();
-    const [article, setArticle] = useState(null);
+  const { id } = useParams();
+  const [article, setArticle] = useState(null);
 
-    useEffect(() =>{
-        fetch(`http://localhost:3000/api/v1/articles/${id}`)
-        .then((res) => res.json())
-        .then((data) => {
-            setArticle(data);
-        });
-    },[id]);
+  useEffect(() => {
+    fetch(`http://localhost:3000/api/v1/articles/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setArticle(data);
+      });
+  }, [id]);
 
-    if(!article){
-        return <div>Cargando...</div>;
-    }
-
+  if (!article) {
     return (
-        <div className="container mx-auto p-4">
-            <h2 className="text-2xl font-semibold text-neutral-900 mb-2">
-                {article.title}
-            </h2>
-            <img src={article.img} alt={article.title} className="w-full object-cover" />
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-purple-600 border-solid"></div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="bg-gray-100">
+      <div className="container mx-auto py-12 px-4">
+        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+          <img src={article.img} alt={article.title} className="w-full h-64 object-cover" />
+          <div className="p-6">
+            <h2 className="text-3xl font-semibold text-purple-800 mb-4">{article.title}</h2>
             {article.article.map((paragraph, i) => (
-                <p key={i} className="text-black text-base mb-2">{paragraph}</p>
+              <p key={i} className="text-gray-800 text-base mb-4">{paragraph}</p>
             ))}
-                  <div className="text-center mt-8">
-        <Link to="/">
-          <button className="bg-purple-500 hover:bg-purple-600 text-white font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline-purple active:bg-purple-700">
-            Volver 
-          </button>
-        </Link>
+            <div className="text-center">
+              <Link to="/">
+                <button className="bg-purple-600 hover:bg-purple-700 text-white font-semibold py-2 px-6 rounded-full focus:outline-none focus:ring-2 focus:ring-purple-600 active:bg-purple-800">
+                  Volver
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-    )
+  );
 }
 
 export default ArticleDetail;
-
