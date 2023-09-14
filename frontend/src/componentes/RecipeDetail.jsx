f97dd5a2de27ec1f32c5a89bede7a1620555f808
Florencia Da Rosa <floordarosa2002gmail.com>
2023-09-14 13:22:06 -0300

    se agregan estilos a recipe detail

--- frontend/src/componentes/RecipeDetail.jsx
+++ frontend/src/componentes/RecipeDetail.jsx
@@ -15,32 +15,43 @@ function RecipeDetail() {
   }, [id]);
 
   if (!recipe) {
-    return <div>Cargando...</div>;
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-purple-600 border-solid"></div>
+      </div>
+    );
   }
 
   return (
-    <div className="container mx-auto p-4">
-      {/* Renderiza los detalles de la receta aquí */}
-      <h2 className="text-2xl font-semibold text-neutral-900 mb-2">
-        {recipe.title}
-      </h2>
-      <img src={recipe.img} alt={recipe.title} className="w-full object-cover" />
-        {recipe.description.map((paragraph, i) => (
-            <p key={i} className="text-black text-base mb-2">{paragraph}</p>
-        ))}
-        <ul className="p-4">
-            {recipe.ingredients.map((ingredient, i) => (
-                <li key={i} className="text-black text-base mb-2">{ingredient}</li>
-            ))}
-        </ul>
-
-      {/* Otros detalles de la receta */}
-      <div className="text-center mt-8">
-        <Link to="/recipes">
-          <button className="bg-purple-500 hover:bg-purple-600 text-white font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline-purple active:bg-purple-700">
-            Volver a recetas
-          </button>
-        </Link>
+    <div className="bg-gray-100">
+      <div className="container mx-auto py-12 px-4">
+        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+          <img src={recipe.img} alt={recipe.title} className="w-full h-64 object-cover" />
+          <div className="p-6">
+            <h2 className="text-3xl font-semibold text-purple-800 mb-4">{recipe.title}</h2>
+            <div className="mb-6">
+              <h3 className="text-xl font-semibold text-purple-700 mb-2">Descripción:</h3>
+              {recipe.description.map((paragraph, i) => (
+                <p key={i} className="text-gray-800 text-base">{paragraph}</p>
+              ))}
+            </div>
+            <div className="mb-6">
+              <h3 className="text-xl font-semibold text-purple-700 mb-2">Ingredientes:</h3>
+              <ul>
+                {recipe.ingredients.map((ingredient, i) => (
+                  <li key={i} className="text-gray-800 text-base mb-2">{ingredient}</li>
+                ))}
+              </ul>
+            </div>
+            <div className="text-center">
+              <Link to="/recipes">
+                <button className="bg-purple-600 hover:bg-purple-700 text-white font-semibold py-2 px-6 rounded-full focus:outline-none focus:ring-2 focus:ring-purple-600 active:bg-purple-800">
+                  Volver a recetas
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
